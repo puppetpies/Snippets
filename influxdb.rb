6f53a506b07b9ebb86f595f88eacacc9902ed493
Bri in The Sky <brianh6854@googlemail.com>
2015-07-21 12:45:49 +0100

    Added Getoptlong for Iterations / Chunksize

--- influxdb.rb
+++ influxdb.rb
@@ -3,16 +3,94 @@
 ########################################################################
 #                                                                      #
 # Author: Brian Hood                                                   #
-# Description: DockerFish Automation                                   #
-# Version: v0.1                                                        #
+# Description: Influx Bench                                            #
+#                                                                      #
+#   Feel free to contribute more to this script / fork                 #
 #                                                                      #
 ########################################################################
+
+=begin
+
+* Hostname was NOT found in DNS cache
+*   Trying 172.17.0.2...
+* Connected to 172.17.0.2 (172.17.0.2) port 8086 (#0)
+> POST /write?db=threatmonitor HTTP/1.1
+> User-Agent: curl/7.37.0
+> Host: 172.17.0.2:8086
+> Accept: */*
+> Content-Length: 43
+> Content-Type: application/x-www-form-urlencoded
+> 
+* upload completely sent off: 43 out of 43 bytes
+< HTTP/1.1 204 No Content
+HTTP/1.1 204 No Content
+< Request-Id: 01be1a93-2e47-11e5-8087-000000000000
+Request-Id: 01be1a93-2e47-11e5-8087-000000000000
+< X-Influxdb-Version: 0.9.1
+X-Influxdb-Version: 0.9.1
+< Date: Sun, 19 Jul 2015 18:50:21 GMT
+Date: Sun, 19 Jul 2015 18:50:21 GMT
+
+< 
+* Connection #0 to host 172.17.0.2 left intact
+
+=end
 
 require "net/http"
 require "uri"
 require "json"
 require "pp"
 require "guid"
+require 'getoptlong'
+
+VERSION = "0.1.1"
+MTABLE = "cpu" # Your test measurements table
+OUTLIMIT = 50 # Limit the read output
+
+ARGV[0] = "--help" if ARGV[0] == nil
+
+opts = GetoptLong.new(
+  [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+  [ '--iterations', '-i', GetoptLong::REQUIRED_ARGUMENT],
+  [ '--chunksize', '-c', GetoptLong::REQUIRED_ARGUMENT],
+  [ '--ommit', '-o', GetoptLong::OPTIONAL_ARGUMENT ]
+)
+
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      helper = "\e[1;34mWelcome to Influx Bench Version #{VERSION}\e[0m\ \n"
+      helper << "\e[1;34m=================================\e[0m\ "
+      helper << %q[
+-h, --help:
+   show help
+
+-i --iterations
+    Iterations of to run
+    
+-c --chunksize
+    Chunksize how many records per iteration
+    
+-o, -ommit 
+    Ommit Select query output to provent total time skew
+
+Example:
+      
+      ruby influxdb.rb -i 3 -c 250
+      
+      This will run 3x 250 record chunks with a total of 750 records being inserted into the database.
+      
+      Also using ommit will stop a flood of on screen data at the end of the write.]
+      puts helper
+      exit
+    when '--iterations'
+      @iterations = arg.to_i
+    when '--chunksize'
+      @chunksize = arg.to_i
+    when '--ommit'
+      @ommit = 1
+  end
+end
 
 module NumTools
 
@@ -59,6 +137,11 @@ class Stopwatch
     @roundvals << round
   end
   
+  def print_stats
+    round = round_to(@t2 - @t1, 2)
+    puts "Start: #{Time.at(@t1)} Finish: #{Time.at(@t2)} Total time: #{round}"
+  end
+  
   def watch(method)
     if method == "start"
       @t1 = timestamp
@@ -69,35 +152,6 @@ class Stopwatch
   end
 
 end
-
-=begin
-
-* Hostname was NOT found in DNS cache
-*   Trying 172.17.0.2...
-* Connected to 172.17.0.2 (172.17.0.2) port 8086 (#0)
-> POST /write?db=threatmonitor HTTP/1.1
-> User-Agent: curl/7.37.0
-> Host: 172.17.0.2:8086
-> Accept: */*
-> Content-Length: 43
-> Content-Type: application/x-www-form-urlencoded
-> 
-* upload completely sent off: 43 out of 43 bytes
-< HTTP/1.1 204 No Content
-HTTP/1.1 204 No Content
-< Request-Id: 01be1a93-2e47-11e5-8087-000000000000
-Request-Id: 01be1a93-2e47-11e5-8087-000000000000
-< X-Influxdb-Version: 0.9.1
-X-Influxdb-Version: 0.9.1
-< Date: Sun, 19 Jul 2015 18:50:21 GMT
-Date: Sun, 19 Jul 2015 18:50:21 GMT
-
-< 
-* Connection #0 to host 172.17.0.2 left intact
-
-=end
-
-OUTLIMIT = 10 # Limit the read output
 
 module DatalayerLight
 
@@ -143,9 +197,10 @@ module DatalayerLight
       begin
         request.body = data unless data.empty?
         response = http.request(request)
-        if response.code == 204 # Good response
-          puts "OK"
-        elsif response.code == 200 or response.code == 400 # 200 can be an error in some cases !!
+        if response.code == "204" # Good response
+          # Lets have to dots.
+          print "."
+        elsif response.code == "200" or response.code == "400" # 200 can be an error in some cases !!
           puts "Error code #{response.code}"
         end
       rescue
@@ -164,24 +219,26 @@ module DatalayerLight
   end
 
 end
-
+# Total Time Stopwatch
+@stwt = Stopwatch.new
+@stwt.watch('start')
+# Select query Stopwatch
 @stw = Stopwatch.new
 metrics = DatalayerLight::InfluxDB.new
 metrics.dbhost = "172.17.0.2"
 begin
   # Write measurements to InfluxDB
-  
   a = ["server1", "server2", "server3"]
   b = ["us-north", "us-south", "us-east", "us-west"]
-  guid = Guid.new # Per benchmark
+  guid = Guid.new # Per benchmark tag
   recordcount = 0
-  5.times {|m|
+  @iterations.times {|m|
     @stw.watch('start')
-    1000.times {|n|
+    @chunksize.times {|n|
       host = a[rand(a.size)]
       region = b[rand(b.size)]
       value = rand(0.99)
-      j = metrics.query("cpu,guid=#{guid},host=#{host},region=#{region} value=#{value}", 'w')
+      j = metrics.query("#{MTABLE},guid=#{guid},host=#{host},region=#{region} value=#{value}", 'w')
       recordcount = recordcount + 1
     }
     puts "#{recordcount} record where written to InfluxDB!"
@@ -197,34 +254,40 @@ begin
   #  WHERE region = 'us-east' LIMIT #{OUTLIMIT}
   @str = Stopwatch.new
   @str.watch('start')
-  j = metrics.query("SELECT * FROM cpu", 'r')
+  j = metrics.query("SELECT * FROM #{MTABLE} WHERE guid = '#{guid}' LIMIT #{OUTLIMIT}", 'r')
   ontheclock = @str.watch('stop')
-  j["results"].each {|n|
-    n["series"].each {|a|
-      name = a["name"]
-      tags = a["tags"]
-      host = tags["host"]
-      region = tags["region"]
-      puts "Tags | Host: #{host} Region: #{region}"
-      columns = a["columns"]
-      values = a["values"]
-      d = 0
-      puts "Dataset"
-      columns.each {|c|
-        values.each {|v|
-          print "#{columns[d]}: #{v[d]} "
-          d = d + 1
-          if d == 2; d = 0; print "\n"; end
+  if !defined?(@ommit)
+    j["results"].each {|n|
+      n["series"].each {|a|
+        name = a["name"]
+        tags = a["tags"]
+        host = tags["host"]
+        region = tags["region"]
+        puts "Tags | Host: #{host} Region: #{region}"
+        columns = a["columns"]
+        values = a["values"]
+        d = 0
+        puts "Dataset"
+        columns.each {|c|
+          values.each {|v|
+            print "#{columns[d]}: #{v[d]} "
+            d = d + 1
+            if d == 2; d = 0; print "\n"; end
+          }
         }
+        puts "Name: #{name} Columns: #{columns}\n\n"
       }
-      puts "Name: #{name} Columns: #{columns}\n\n"
     }
-  }
   puts "Data output complete ! i am limited to #{OUTLIMIT} records in the code"
+  else
+    puts "Query output ommited"
+  end
 rescue
   puts "Unable to read data"
 end
+@stwt.watch('stop')
 
+# Use at_exit for a good reason
 at_exit {
   include NumTools
   mybench = @stw.roundvals
@@ -233,8 +296,9 @@ at_exit {
   avground = round_to(avg, 2)
   max = mybench.max
   arrsize = mybench.size
-
   mybench2 = @str.roundvals
   puts "Select Query: #{mybench2}"
   puts "Insert Performance - Min: #{min} Avg: #{avground} Max: #{max} Arr Size: #{arrsize}"
+  # Display Total Runtime
+  @stwt.print_stats
 }
