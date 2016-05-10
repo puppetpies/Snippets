54107a13f6ffb59922adabc47c1deed1a390f07f
Bri in The Sky <brianh6854@googlemail.com>
2016-05-10 21:51:08 +0100

    Method Examples In Ruby

---
+++ methods_examples.rb
@@ -0,0 +1,45 @@
+
+class MyTest
+
+  class << self
+  
+    def singleton
+      puts "Singleton method"
+    end
+  
+  end
+  
+  private
+  
+  def priv_test
+    puts "Private Test: Private method I'm only available in current scope"
+  end
+  
+  public
+
+  def pub_test_1
+    puts "Public method lookup of : priv_test: Private method"
+    priv_test
+  end
+  
+  def pub_test_2
+    puts "Public method lookup of : prot_test: Protected method"
+    prot_test
+  end
+
+  protected
+  
+  def prot_test
+    puts "Protected method lookup of : pub_test_1: Public method"
+    pub_test_1
+    puts "Protected method lookup of : priv_test: Private Method"
+    priv_test
+  end
+
+end
+
+my = MyTest.new
+my.pub_test_1
+my.pub_test_2
+
+MyTest.singleton
