938600659646c7dfb9ca6e1f1b313a98f979b243
Bri in The Sky <brianh6854@googlemail.com>
2016-07-20 13:47:13 +0100

    Fixed example will submit patch later to thor tail.cr project

---
+++ tail.cr
@@ -0,0 +1,19 @@
+module Tail
+  def self.tail(target_path, delay=1, &block)
+    last_line_count = File.read_lines(target_path).size
+    loop do
+      content = File.read_lines(target_path)
+      line_count = content.size
+      if line_count > last_line_count
+        new_lines = content[last_line_count..-1]
+        yield new_lines[0]
+      end
+      last_line_count = line_count
+      sleep delay
+    end
+  end
+end
+
+Tail.tail "/tmp/mytest.txt" do |n|
+ puts n
+end
